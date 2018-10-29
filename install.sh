f9d13bfa9a032c35c036d022877389bab7b1479f
profionaldhim <35074237+profionaldhim@users.noreply.github.com>
2018-10-30 00:04:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,14 @@
 #!bin/bash
-
+mkdir output
 apt update && apt upgrade
 pkg install figlet -y
-pkg install ruby -y
-pkg install gem -y
 pkg install toilet -y
-pkg install lolcat -y
+gem install lolcat -y
 pip2 install requests
 pkg install python -y
 pkg install python2 -y
 pkg install python3 -y
 pkg install git -y
 pkg install curl -y
-pkg install wgt -y
-gem install figlet
-apt-get install figlet
-apt install lolcat -y
-apt install python -y
-apt install python2 -y
-cd ~
-git clone https://github.com/sqlmapproject/sqlmap
-cd f-inection
+pkg install wget -y
 chmod +x Fastinjection.sh
